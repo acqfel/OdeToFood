d02091a8bfaf05e2a0d9301b4be3d48f63fbe01b
Felipe Acquati <felipe_acq@yahoo.com.br>
2020-02-16 21:49:10 -0300

    Added: Core & Data Layer; Table do displayed restaurant data to List Page.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+	public interface IRestaurantData
+	{
+		IEnumerable<Restaurant> GetAll();
+	}
+
+	public class InMemoryRestaurantData : IRestaurantData
+	{
+		List<Restaurant> restaurants;
+
+		public InMemoryRestaurantData()
+		{
+			restaurants = new List<Restaurant>()
+			{ 
+				new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian },
+				new Restaurant { Id = 2, Name = "Andy's Taco", Location = "Miami", Cuisine = CuisineType.Mexican },
+				new Restaurant { Id = 1, Name = "Indian Food X", Location = "New York", Cuisine = CuisineType.Indian }
+			};
+		}
+
+		public IEnumerable<Restaurant> GetAll()
+		{
+			return from r in restaurants
+				   orderby r.Name
+				   select r;
+		}
+	}
+}
